fb0afc07c4c6f22b288dc314ed0ffdf84db4358f
Catherine Li <catherine.li34@gmail.com>
2020-11-29 11:04:34 -0800

    Added input form that displays new tasks

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 var currentTodo = {
     "todos": []
 };
+function addTaskToList() {
+    var taskTitle = document.getElementById("todo-title").value;
+    var taskDescription = document.getElementById("todo-description").value;
+    addTask(taskTitle, taskDescription);
+}
 function addTask(title, description) {
     var newTask = {
         "title": title,
         "description": description
     };
     currentTodo.todos.push(newTask);
+    displayTasks();
 }
 function deleteTask() {
     currentTodo.todos.pop();
@@ -15,5 +21,18 @@ function returnTasks() {
     currentTodo.todos.forEach(function (element) {
         console.log("Title: " + element.title);
         console.log("Description: " + element.description);
+    });
+}
+function displayTasks() {
+    var listDiv = document.getElementById("todo-list");
+    currentTodo.todos.forEach(function (element) {
+        var todo = document.createElement("div");
+        var title = document.createElement("p");
+        var description = document.createElement("p");
+        title.innerHTML = element.title;
+        description.innerHTML = element.description;
+        todo.appendChild(title);
+        todo.appendChild(description);
+        listDiv === null || listDiv === void 0 ? void 0 : listDiv.appendChild(todo);
     });
 }
--- script.ts
+++ script.ts
@@ -11,6 +11,12 @@ var currentTodo: Todo = {
     "todos": []
 };
 
+function addTaskToList(): void {
+    let taskTitle: string = (<HTMLInputElement>document.getElementById("todo-title")).value;
+    let taskDescription: string = (<HTMLInputElement>document.getElementById("todo-description")).value;
+    addTask(taskTitle, taskDescription);
+} 
+
 function addTask(title: string, description: string): void {
     let newTask:Task = {
         "title": title,
@@ -18,6 +24,7 @@ function addTask(title: string, description: string): void {
     };
 
     currentTodo.todos.push(newTask);
+    displayTasks();
 }
 
 function deleteTask(): void {
@@ -28,5 +35,22 @@ function returnTasks(): void {
     currentTodo.todos.forEach(element => {
         console.log("Title: " + element.title);
         console.log("Description: " + element.description);
+    });
+}
+
+function displayTasks(): void {
+    const listDiv = document.getElementById("todo-list");
+    currentTodo.todos.forEach(element => {
+        const todo = document.createElement("div")
+        const title = document.createElement("p")
+        const description = document.createElement("p")
+
+        title.innerHTML = element.title;
+        description.innerHTML = element.description;
+
+        todo.appendChild(title)
+        todo.appendChild(description)
+
+        listDiv?.appendChild(todo)
     });
 }
